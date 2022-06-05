48c01eae2965f7425d8c816675d92a10b18ae4cb
xlwnya <xlwnya@ggtea.org>
2022-06-06 01:13:28 +0900

    SaveRenderTexture.cs, ShowProjection.cs

---
+++ Camera/Scripts/SaveRenderTexture.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Xlwnya.Misc.Camera.Scripts
+{
+    public class SaveRenderTexture : MonoBehaviour
+    {
+        public enum SaveFormat
+        {
+            EXR,
+            PNG,
+            JPG,
+            TGA
+        }
+    
+        public RenderTexture targetRT;
+        public SaveFormat saveFormat;
+        public TextureFormat saveTextureFormat = TextureFormat.RGBAFloat;
+        public bool saveLinear = true;
+        public bool exrOutputAsFloat = true;
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(SaveRenderTexture))]
+    public class SaveRenderTextureEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var self = (SaveRenderTexture)target;
+            
+            serializedObject.Update();
+
+            bool execSaveRT = false;
+            if (self.targetRT) execSaveRT = GUILayout.Button("Save RenderTexture");
+
+            DrawDefaultInspector();
+            
+            if (execSaveRT) SaveRT();
+            
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void SaveRT()
+        {
+            var self = (SaveRenderTexture)target;
+
+            string ext = self.saveFormat.ToString().ToLower();
+            string path = EditorUtility.SaveFilePanelInProject("Save RenderTexture", "texture", ext, "");
+            if (path.Length > 0)
+            {
+                Texture2D tex = GetRTPixels(self.targetRT, self.saveTextureFormat, self.saveLinear);
+                byte[] data;
+                switch (self.saveFormat)
+                {
+                    case SaveRenderTexture.SaveFormat.EXR:
+                        Texture2D.EXRFlags flags = Texture2D.EXRFlags.CompressZIP;
+                        if (self.exrOutputAsFloat) flags &= Texture2D.EXRFlags.OutputAsFloat;
+                        data = tex.EncodeToEXR(flags);
+                        break;
+                    
+                    case SaveRenderTexture.SaveFormat.PNG:
+                        data = tex.EncodeToPNG();
+                        break;
+                    
+                    case SaveRenderTexture.SaveFormat.JPG:
+                        data = tex.EncodeToJPG();
+                        break;
+                    
+                    case SaveRenderTexture.SaveFormat.TGA:
+                        data = tex.EncodeToTGA();
+                        break;
+                    default:
+                        throw new Exception("unknown SaveFormat.");
+                }
+                File.WriteAllBytes(path, data);
+                AssetDatabase.ImportAsset(path);
+            }
+        }
+        
+        // https://docs.unity3d.com/jp/current/ScriptReference/RenderTexture-active.html
+        static private Texture2D GetRTPixels(RenderTexture rt, TextureFormat saveFormat, bool linear)
+        {
+            // Remember currently active render texture
+            RenderTexture currentActiveRT = RenderTexture.active;
+
+            // Set the supplied RenderTexture as the active one
+            RenderTexture.active = rt;
+
+            // Create a new Texture2D and read the RenderTexture image into it
+            Texture2D tex = new Texture2D(rt.width, rt.height, saveFormat, false, linear);
+            tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+
+            // Restorie previously active render texture
+            RenderTexture.active = currentActiveRT;
+            return tex;
+        }
+    }
+#endif
+}
---
+++ Camera/Scripts/ShowProjection.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Xlwnya.Misc.Camera.Scripts
+{
+    [ExecuteInEditMode]
+    public class ShowProjection : MonoBehaviour
+    {
+        public Matrix4x4 cameraMatrix;
+        public Vector2 cameraSize;
+        public Vector2 clipPlane;
+        public float aspect;
+        public float orthographicSize;
+
+        public Material targetMaterial;
+        
+        private UnityEngine.Camera _cam;
+        
+#if UNITY_EDITOR
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (!_cam) _cam = GetComponent<UnityEngine.Camera>();
+        }
+
+        private void OnEnable()
+        {
+            if (!_cam) _cam = GetComponent<UnityEngine.Camera>();
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            cameraMatrix = _cam.worldToCameraMatrix;
+            orthographicSize = _cam.orthographicSize;
+            aspect = _cam.aspect;
+            cameraSize = new Vector2(orthographicSize * aspect, orthographicSize);
+            clipPlane = new Vector2(_cam.nearClipPlane, _cam.farClipPlane);
+        }
+
+        private void CopyCameraProjection()
+        {
+            if (!_cam || !targetMaterial) return;
+            if (targetMaterial.HasProperty("_CameraMatrixR0")) targetMaterial.SetVector("_CameraMatrixR0", cameraMatrix.GetRow(0));
+            if (targetMaterial.HasProperty("_CameraMatrixR1")) targetMaterial.SetVector("_CameraMatrixR1", cameraMatrix.GetRow(1));
+            if (targetMaterial.HasProperty("_CameraMatrixR2")) targetMaterial.SetVector("_CameraMatrixR2", cameraMatrix.GetRow(2));
+            if (targetMaterial.HasProperty("_CameraMatrixR3")) targetMaterial.SetVector("_CameraMatrixR3", cameraMatrix.GetRow(3));
+            if (targetMaterial.HasProperty("_CameraSize"))
+            {
+                targetMaterial.SetVector("_CameraSize", new Vector4(orthographicSize*_cam.aspect, orthographicSize, 1, 1));
+            }
+            if (targetMaterial.HasProperty("_NearClipPlane")) targetMaterial.SetFloat("_NearClipPlane", _cam.nearClipPlane);
+            if (targetMaterial.HasProperty("_FarClipPlane")) targetMaterial.SetFloat("_FarClipPlane", _cam.farClipPlane);
+        }
+
+        [CustomEditor(typeof(ShowProjection))]
+        public class ShowProjectionEditor : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                var self = (ShowProjection) target;
+                
+                serializedObject.Update();
+                var iter = serializedObject.GetIterator();
+                iter.NextVisible(true);
+                while(iter.NextVisible(false))
+                {
+                    EditorGUILayout.PropertyField(iter, true);
+                    if (iter.name == "cameraMatrix")
+                    {
+                        // 行列の内容を行単位で出力しておく
+                        EditorGUILayout.Vector4Field("cameraMatrixR0", self.cameraMatrix.GetRow(0));
+                        EditorGUILayout.Vector4Field("cameraMatrixR1", self.cameraMatrix.GetRow(1));
+                        EditorGUILayout.Vector4Field("cameraMatrixR2", self.cameraMatrix.GetRow(2));
+                        EditorGUILayout.Vector4Field("cameraMatrixR3", self.cameraMatrix.GetRow(3));
+                    }
+                }
+                serializedObject.ApplyModifiedProperties();
+                
+                bool execCopy = GUILayout.Button("Copy camera data to Material");
+                if (execCopy) self.CopyCameraProjection();
+            }
+        }
+#endif
+    }
+}
